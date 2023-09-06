b909b9e1ad61b3fa72675029e3fe5a2c02c417da
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-09-06 18:18:02 +0100

    nothing

--- src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/ImageController.java
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/ImageController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/images")
 @RequiredArgsConstructor
