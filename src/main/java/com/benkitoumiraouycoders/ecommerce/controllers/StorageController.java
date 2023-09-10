ef980325d32c7addcb10d9e009546747009be768
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-09-10 22:20:06 +0100

    api for getting images from S3

---
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/StorageController.java
@@ -0,0 +1,56 @@
+package com.benkitoumiraouycoders.ecommerce.controllers;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.benkitoumiraouycoders.ecommerce.services.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+    @Autowired
+    private StorageService service;
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file, 1L), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+
+    @GetMapping("/{productId}/images")
+    public List<String> getProductImages(@PathVariable Long productId) {
+        return service.getProductImages(productId);
+    }
+
+
+}
---
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/services/StorageService.java
@@ -0,0 +1,92 @@
+package com.benkitoumiraouycoders.ecommerce.services;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class StorageService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file, Long productId) {
+        String folderName = "products/product-" + productId;
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+        // Check if the products folder exists, and create it if not
+        if (!s3Client.doesObjectExist(bucketName, folderName)) {
+            s3Client.putObject(bucketName, folderName, "");
+        }
+
+        // Upload the file to the specified folder
+        String key = folderName + "/" + fileName;
+        s3Client.putObject(new PutObjectRequest(bucketName, key, convertMultiPartFileToFile(file)));
+
+        return "File uploaded: " + key;
+    }
+
+
+    public byte[] downloadFile(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public String deleteFile(String fileName) {
+        s3Client.deleteObject(bucketName, fileName);
+        return fileName + " removed ...";
+    }
+
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
+
+    public List<String> getProductImages(Long productId) {
+        String folderName = "products/product-" + productId;
+
+        // List objects in the specified folder
+        ListObjectsV2Request listObjectsRequest = new ListObjectsV2Request()
+                .withBucketName(bucketName)
+                .withPrefix(folderName + "/");
+        ListObjectsV2Result objectListing = s3Client.listObjectsV2(listObjectsRequest);
+
+        List<String> imageUrls = new ArrayList<>();
+
+        // Extract image URLs
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            imageUrls.add(s3Client.getUrl(bucketName, objectSummary.getKey()).toString());
+        }
+
+        return imageUrls;
+    }
+}
