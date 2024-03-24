75f859b41a1a97774737699f9c40dd2c11a4fca5
othmane <othmaneouacrim@gmail.com>
2024-03-24 19:54:35 +0000

    Add api in UpsellController

--- src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/UpsellController.java
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/UpsellController.java
@@ -1,17 +1,21 @@
 package com.benkitoumiraouycoders.ecommerce.controllers;
 
 
+import com.benkitoumiraouycoders.ecommerce.dtos.UpsellDto;
 import com.benkitoumiraouycoders.ecommerce.entities.Upsell;
 import com.benkitoumiraouycoders.ecommerce.services.UpsellServiceImpl;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/upsell")
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:58213", allowCredentials = "true")
 public class UpsellController {
 
     private final UpsellServiceImpl upsellService;
@@ -21,4 +25,29 @@ public class UpsellController {
             @RequestParam(defaultValue = "10") int size) {
         return upsellService.getUpsells(PageRequest.of(page, size));
     }
+
+    @GetMapping("/{upsellId}")
+    public ResponseEntity<UpsellDto> getUpsellById(@PathVariable Long upsellId) {
+        return ResponseEntity.ok().body(upsellService.getUpsellById(upsellId));
+    }
+
+    @PostMapping
+    public ResponseEntity<UpsellDto> addUpsell(
+            @RequestBody UpsellDto upsellDto) throws IOException {
+        return ResponseEntity.ok().body(upsellService.addUpsell(upsellDto));
+    }
+
+
+    @PutMapping("/{upsellId}")
+    public ResponseEntity<UpsellDto> updateUpsell
+            (@PathVariable Long upsellId, @RequestBody UpsellDto upsellDto) {
+        return ResponseEntity.ok().body(upsellService.updateUpsell(upsellId, upsellDto));
+    }
+
+    @DeleteMapping("/{upsellId}")
+    public ResponseEntity<?> deleteClientById(@PathVariable Long upsellId) {
+        return ResponseEntity.ok().body(upsellService.deleteUpsellById(upsellId));
+    }
+
+
 }
