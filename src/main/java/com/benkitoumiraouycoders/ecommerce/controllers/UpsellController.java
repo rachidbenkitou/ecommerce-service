2479a61ebde8fb4eb9039f5ea499fca79da9bf78
othmane <othmaneouacrim@gmail.com>
2024-03-28 09:41:33 +0000

    add documentation and implement service Supplierproductservice

--- src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/UpsellController.java
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/controllers/UpsellController.java
@@ -4,7 +4,10 @@ package com.benkitoumiraouycoders.ecommerce.controllers;
 import com.benkitoumiraouycoders.ecommerce.dtos.UpsellDto;
 import com.benkitoumiraouycoders.ecommerce.entities.Upsell;
 import com.benkitoumiraouycoders.ecommerce.services.UpsellServiceImpl;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.openapi.models.responses.APIResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/upsell")
 @AllArgsConstructor
@@ -23,17 +27,20 @@ public class UpsellController {
     public Page<Upsell> getAllUpsells(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
+        log.info("UpsellController::getAllUpsells Fetching Allupsells  {}.",PageRequest.of(page,size));
         return upsellService.getUpsells(PageRequest.of(page, size));
     }
 
     @GetMapping("/{upsellId}")
     public ResponseEntity<UpsellDto> getUpsellById(@PathVariable Long upsellId) {
+        log.info("UpsellController::getUpsellById Fetching upsell ById with id: {} .", upsellId);
         return ResponseEntity.ok().body(upsellService.getUpsellById(upsellId));
     }
 
     @PostMapping
     public ResponseEntity<UpsellDto> addUpsell(
             @RequestBody UpsellDto upsellDto) throws IOException {
+        log.info("UpsellController::addUpsell response {}", upsellDto);
         return ResponseEntity.ok().body(upsellService.addUpsell(upsellDto));
     }
 
@@ -41,11 +48,13 @@ public class UpsellController {
     @PutMapping("/{upsellId}")
     public ResponseEntity<UpsellDto> updateUpsell
             (@PathVariable Long upsellId, @RequestBody UpsellDto upsellDto) {
+        log.info("UpsellController::updateUpsell response ");
         return ResponseEntity.ok().body(upsellService.updateUpsell(upsellId, upsellDto));
     }
 
     @DeleteMapping("/{upsellId}")
     public ResponseEntity<?> deleteClientById(@PathVariable Long upsellId) {
+        log.info("UpsellController::deleteClientById deleteClientById{} ",upsellId);
         return ResponseEntity.ok().body(upsellService.deleteUpsellById(upsellId));
     }
 }
--- src/main/java/com/benkitoumiraouycoders/ecommerce/dao/SupplierProductDao.java
+++ src/main/java/com/benkitoumiraouycoders/ecommerce/dao/SupplierProductDao.java
@@ -1,10 +1,13 @@
 package com.benkitoumiraouycoders.ecommerce.dao;
 
 import com.benkitoumiraouycoders.ecommerce.entities.SupplierProduct;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SupplierProductDao extends JpaRepository<SupplierProduct, Long> {
     List<SupplierProduct> findBySupplierId(Long supplierId);
-}
+
+
+   }
